50e905f49e1a3350592eb253fd0f49f9b4befd0a
Shreekavya <shreekavyats@gmail.com>
2014-10-11 15:34:04 +0530

    initial first app

---
+++ hello-world/ctrl.js
@@ -0,0 +1,20 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('ABC')
+        .controller('ABCCtrl', Ctrl);
+
+    /* @ngInject */
+    function Ctrl($scope) {
+        /*jshint validthis: true */
+        var vm = this;
+        vm.title = 'Ctrl';
+
+       	$scope.helloText = 'Hello World!';
+        activate();
+
+        function activate() {
+        }
+    }
+})();
